69be17d33546fac6d48bf2442b21b146af57b57b
NawodaWadisingha <146326702+NawodaWadisingha@users.noreply.github.com>
2023-10-27 21:38:42 +0530

    Add files via upload

---
+++ Week 02/Additional_Exercises.py
@@ -0,0 +1,10 @@
+print("Convert an input Centigrade temperatre into Fahrenheit")
+temp = int(input("Enter the temperature in Centigrade: "))
+print("Temperature in Fahrenheit: ", (temp*1.8)+32)
+
+print("calculate the volume of a box")
+length = int(input("Enter values for the length: "))
+height = int(input("Enter values for the height: "))
+width = int(input("Enter values for the width: "))
+volume = length*height*width
+print("Volume of a box: ",volume)
