25fe44a5f669aa06aa71c0ff2b79330a875a3d5a
NawodaWadisingha <146326702+NawodaWadisingha@users.noreply.github.com>
2023-10-29 00:00:08 +0530

    Add files via upload

---
+++ Week 04/Exercise 6.py
@@ -0,0 +1,4 @@
+num = int(input("How many stars (*) are required: "))
+for i in range(num):
+    print("*",end = " ")
+ 
---
+++ Week 04/Exercise 8.py
@@ -0,0 +1,19 @@
+#A
+for number in range(3) : 
+    print("-------------------------------------------")
+    print("Outer loop iteration " + str(number))
+    # Inner loop
+    for another_number in range(4): 
+        print("****************************")
+        print("In inner loop iteration " + str(another_number))
+
+#B
+for x in range(1,4): # print 3 rows
+    for y in range(1,4): # 3 asterisks a row
+        print('*', end='')
+    print() 
+
+#C
+for i in range(1,4):
+    print("*"*i)
+        
